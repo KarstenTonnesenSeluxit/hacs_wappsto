88a20e18c0b01e2d7f292c439a3a7735bf3ca466
Karsten Tonnesen <karsten@seluxit.com>
2023-08-02 11:08:28 +0200

    lots of tests.

--- custom_components/wappsto/config_flow.py
+++ custom_components/wappsto/config_flow.py
@@ -1,28 +1,30 @@
+import logging
+
 from homeassistant import config_entries
 from .const import DOMAIN
 from homeassistant.data_entry_flow import FlowResult
 
-from homeassistant.const import (
+from .const import (
     CONF_HOST,
     CONF_NAME,
+    DOMAIN,
 )
 
+_LOGGER = logging.getLogger(__name__)
 
-# class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class WappstoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Example config flow."""
+    # The schema version of the entries that it creates
+    # Home Assistant will call your migrate method if the version changes
+    _LOGGER.error("ConfigFlow TEST WappstoConfigFlow")
+    VERSION = 1
 
-#     async def async_step_user(
-#         self, user_input: dict[str, Any] | None = None
-#     ) -> FlowResult:
-#         """Handle a flow initialized by the user."""
-#         errors: dict[str, str] = {}
-#         if user_input is not None:
-#             self._host = user_input[CONF_HOST]
-#             self._name = user_input[CONF_NAME]
-#             return await self.async_step_pairing()
+    def __init__(self):
+        _LOGGER.warning("Init ConfibFLow")
 
-#         return self.async_show_form(
-#             step_id=step_id,
-#             data_schema=data_schema,
-#             errors=errors or {},
-#             description_placeholders=description_placeholders,
-#         )
+class WappstoOptionsConfigFlow(config_entries.Optionsflow)
+    _LOGGER.error("ConfigFlow TEST WappstooptionsConfigFlow")
+
+    def __init__(self):
+        _LOGGER.warning("Init OptionsConfigFlow")
+
